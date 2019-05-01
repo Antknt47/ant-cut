1cb52a36e67f189b6147c7e1664cd3f9ddef8375
Antknt47 <403586128@qq.com>
2019-05-01 12:48:58 +0800

    blank demo

---
+++ src/renderer/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+// bootstrap
+Vue.use(BootstrapVue)
+
+/* eslint-disable no-new */
+
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
